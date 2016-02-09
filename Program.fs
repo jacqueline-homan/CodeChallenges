f641eb6149f2a98e28f70ebdb64151c753bf5f77
Jacqueline Homan <jacquelinehoman7@gmail.com>
2016-02-09 08:24:14 -0500

    Added Riemann zeta function demo

--- Program.fs
+++ Program.fs
@@ -12,6 +12,7 @@ let printNumbers() =
     printfn "%d" (a+b)
     printfn "%d" (a*b)
 
+//Pattern-matching using a "when" guard to establish rules for matching
 let nums = [1..100] 
 nums
 |> Seq.map (function
@@ -21,6 +22,7 @@ nums
     | x -> string x)
 |> Seq.iter (printfn "%s")
 
+//Pattern-matching as part of a function that the original function was pipelined into
 let nums2 = [1..100]
 nums2
 |> Seq.map (fun x->
@@ -31,6 +33,7 @@ nums2
     | _ -> string x)
 |> Seq.iter (printfn "%s")
 
+//Pattern-matching on the parameters of a function
 let fizzbuzz modFizz modBuzz = 
     [1..100]
     |> Seq.map (fun x->
@@ -42,6 +45,7 @@ let fizzbuzz modFizz modBuzz =
 fizzbuzz 3 5
 |> Seq.iter (printfn "%s")
 
+//Passing functions as parameters in another function
 let fizzBuzz modFizz modBuzz = 
     [1..100]
     |> Seq.map (fun x->
@@ -53,9 +57,21 @@ let fizzBuzz modFizz modBuzz =
 fizzBuzz (fun x -> x%3) (fun x -> x%5)
 |> Seq.iter (printfn "%s")
 
- 
-
-
+(*Demonstrating the power of F# in handling complicated algorithms
+Here we compute the limit of an infinite series by applying Cauchy's theorem
+to the Riemann zeta function which is a function of a complex variable, k,
+that analytically continues the sum of the infinite series, which converges to
+a limit of 1 when the real part of k is greater than 1.
+*)
+let zeta k e =
+    let inner(i) = 1.0/(double(i)**k)
+    let rec seqInfinite i xp =
+        let x = xp + inner(i)
+        match abs(xp - x) with
+        | dx when dx <= e -> x
+        | _ -> seqInfinite(i + 1) x
+    seqInfinite 1 0.0
+printfn "%f" (zeta 15.0 0.0)
 
 [<EntryPoint>]
 let main argv =
